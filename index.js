62bd879071b6fabed8df6d24663fe9b5f48b01b0
Cody S <cody.scoles@gmail.com>
2022-03-04 11:00:19 -0500

    Add functions to generateMarkdown.js. Update validation.

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-// TODO: Include packages needed for this application
+// Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown');
 
 
-// TODO: Create an array of questions for user input
+// Create an array of questions for user input
 const questions = [
+    //Get project title
     {
         type: 'input',
         name: 'title',
         message: 'Project title: ',
     },
+    //Get user email
     {
         type: 'input',
         name: 'emailAddress',
@@ -19,59 +21,79 @@ const questions = [
             const pass = value.match(/[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/)
             if (pass) {
                 return true;
-              }
-        
-              return 'Please enter a valid email.';
-            },
-        
+            }
+
+            return 'Please enter a valid email.';
+        },
+
     },
+    //Get user Github username
     {
         type: 'input',
         name: 'gitHub',
         message: 'Enter your GitHub username: ',
         validate(value) {
-            const pass = value.match(/w+/i)
+            const pass = value.match(/^[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}$/i)
             if (pass) {
                 return true;
-              }
-        
-              return 'Please enter a valid GitHub username.';
-            },
-        
+            }
 
-        
+            return 'Please enter a valid GitHub username.';
+        },
     },
+    //Get project description
     {
         type: 'input',
         name: 'description',
         message: 'Enter a brief description for the project: ',
     },
+    //Get user defined usage section
     {
         type: 'editor',
         name: 'usage',
         message: 'Enter usage information, in markdown: ',
     },
+    //Get project contributors
     {
         type: 'input',
         name: 'contributors',
         message: 'Enter all collaborators on the project, separated by a comma: ',
-        
+
+    },
+    //Get license
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Please select a license for the project.',
+        choices: ['Apache License 2.0', 'GNU General Public License v3.0', 'MIT License',
+                  'BSD 2-Clause "Simplified" License', 'BSD 3-Clause "New" License',
+                  'Boost Software License 1.0', 'Creative Commons Zero v1.0 Universal',
+                  'Eclipse Public License 2.0', 'GNU Affero General Public License v3.0', 
+                  'GNU General Public License v2.0', 'GNU Lesser General Public License v2.1', 
+                  'Mozilla Public License 2.0', 'The Unlicense'],
+        validate(value){
+            if (value) {
+                return true;
+            }
+
+            return 'You must select a license.';
+        }
     },
 ];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) { 
+// Writes the README file
+function writeToFile(fileName, data) {
     fs.writeFile(fileName, data, (err) => console.error('Error', err));
 }
 
-// TODO: Create a function to initialize app
-function init() { 
+// Initializes the app
+function init() {
     inquirer
-    .prompt(questions)
-    .then(data => {
-        writeToFile(`${data.title}.md`, generateMarkdown(data));
-    }
-    )
+        .prompt(questions)
+        .then(data => {
+            writeToFile(`${data.title}.md`, generateMarkdown(data));
+        }
+        )
 };
 
 // Function call to initialize app
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,84 +1,155 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
+/**************************************************************************/
+/*****      The following functions define the README sections        *****/
+/**************************************************************************/
+
+
+// This function return a license badge based on which license is passed in
+// If there is no license, it returns an empty string
 function renderLicenseBadge(license) {
-  if(!license){
+  if (!license) {
     return '';
   }
-  return `![License](${renderLicenseLink()})`
+  return `${renderLicenseLink(license)}`
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
+// This function returns the license link
+// If there is no license, it returns an empty string
 function renderLicenseLink(license) {
-  if(!license){
-    return '';
+  if (!license) {
+    return ''
+  }
+
+  switch (license) {
+    case 'Apache License 2.0':
+      return '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+    case 'GNU General Public License v3.0':
+      return '[![License](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+    case 'MIT License':
+      return '[![License](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+    case 'BSD 2-Clause "Simplified" License':
+      return '[![License](https://img.shields.io/badge/License-BSD%202--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)';
+    case 'BSD 3-Clause "New" License':
+      return '[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)';
+    case 'Boost Software License 1.0':
+      return '[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)';
+    case 'Creative Commons Zero v1.0 Universal':
+      return '[![License](https://img.shields.io/badge/License-CC0%201.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)';
+    case 'Eclipse Public License 2.0':
+      return '[![License](https://img.shields.io/badge/License-EPL%202.0-red.svg)](https://opensource.org/licenses/EPL-2.0)';
+    case 'GNU Affero General Public License v3.0':
+      return '[![License](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)';
+    case 'GNU General Public License v2.0':
+      return '[![License](https://img.shields.io/badge/License-GPL%20v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)';
+    case 'GNU Lesser General Public License v2.1':
+      return '[![License](https://img.shields.io/badge/License-LGPL%20v2.1-blue.svg)](https://www.gnu.org/licenses/lgpl-2.1)';
+    case 'Mozilla Public License 2.0':
+      return '[![License](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)';
+    case 'The Unlicense':
+      return '[![License](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)';
+    default:
+      return '';
   }
 }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
+// This function returns the license section of README
+// If there is no license, it returns an empty string
 function renderLicenseSection(license) {
-  `#License
-    ${renderLicenseBadge()}`
+  return `## License 
+
+  ${renderLicenseBadge(license)}`
 }
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  contributors = data.contributors.split(',').join('\n * ');
-  return `# ${data.title}
+// This function returns the description section of README
+// If there is no description, it returns an empty string
+function renderDescription(description) {
+  if (description) {
+   return  `## Description
+  ${description}`
+  }
+}
 
-  ## Description
-  ${data.description}
-
-  ## TOC
+// This function returns the TOC section of README
+function renderTOC() {
+  return `## TOC
   1. [Installation](#installation)   
   2. [Usage](#usage)                 
   3. [Contributors](#contributors)
   4. [Tests](#tests)
-  5. [Questions](#questions)
+  5. [Questions](#questions)`
+}
 
-  ## Installation
+// This function returns the Installation section of README
+function renderInstallation(title, username, repo) {
+  return `## Installation
   ***
-  Type these commands into git bash to install the application.
+  Type these commands into git bash to install the application. ${title} was created with
+  [git bash](https://git-scm.com/) and [node package manager](https://nodejs.org/en/).
+
   \`\`\`
-  $ git clone git@github.com:YellowYam/good-readme-generator.gitgit
+  $ git clone git@github.com:${username}/${repo}.git
   $ cd ../path/to/the/file
   $ npm install
-  $ npm start
-  \`\`\`
-  
+  \`\`\` `
+}
 
-  ## Usage
-  Good-README-generator is a node-based JavaScript file. After installation, open a bash shell, navigate
-  to the generator's directory, and run <code> node index.js </code>.
-  
-  A prompt will appear in the console window.
-  <!-- [!Example prompt image](./prompt-screenshot.jpg) -->
+// This function returns the Usage section of README
+// If there is no Usage section, it returns an empty string
+function renderUsage(usage) {
+  if (usage !== '') {
+    return `## Usage
+   ${usage}`
+  }
+}
 
-  Answer the questions by entering all required information. The application writes a markdown file to the 
-  working directory with the same filename as the title of the new project.
-
-  <!-- [!Example readme file](./prompt-screenshot-2.jpg) -->
-
-  This file contains a functional professional README. Be sure to add any additional tests and features as
-  they're developed.
-
-  ## Contributors
+// This function returns the Contributors section of README
+function renderContributors(contributors) {
+ return `## Contributors
   If you would like to become a contributor on this project, please find my contact information in the [questions](#questions)
   section of this README.
 
-  * ${contributors}
+  * ${contributors}`
+}
 
-  ## Tests
-  At present, no tests can be performed on this code. The application meets all requirements without errors.
-  ![Issues](https://img.shields.io/github/issues/YellowYam/good-readme-generator?style=plastic)
-  ![Tests](https://img.shields.io/appveyor/tests/YellowYam/good-readme-generator/main?style=plastic)
+// This function returns the Tests section of README
+// It shows all open issues in any Github repo with the project title.
+function renderTests(username, repo) {
+  return `## Tests
+  ![Issues](https://img.shields.io/github/issues/${username}/${repo}?style=plastic)<br>
+  At present, no tests can be performed on this code. The application meets all requirements without errors.`
+}
 
-  ## Questions?
-  <a href = "mailto:${data.emailAddress}"> Email me! </a> <br>
-  <a href = "https://www.github.com/${data.gitHub}"> GitHub </a>
+// This function returns the Contributors section of README
+function renderQuestions(email, github) {
+ return  `## Questions?
+  <a href = "mailto:${email}"> Email me! </a> <br>
+  <a href = "https://www.github.com/${github}"> GitHub </a>`
+}
 
-  ${renderLicenseSection}
+/**********************************************/
+// This function generates markdown for README
+function generateMarkdown(data) {
+
+  //Special formatting
+  contributors = data.contributors.split(',').join('\n * ');
+  repoTitle = data.title.split(' ').join('-');
+
+  return `# ${data.title}
+  
+  ${renderDescription(data.description)}
+
+  ${renderTOC()}
+
+  ${renderInstallation(data.title, data.gitHub, repoTitle)}
+
+  ${renderUsage(data.usage)}
+
+  ${renderContributors(contributors)}
+
+  ${renderTests(data.gitHub, repoTitle)}
+
+  ${renderQuestions(data.email, data.gitHub)}
+
+  ${renderLicenseSection(data.license)}
 
 `;
 }
